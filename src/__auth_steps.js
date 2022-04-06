b4f17cc7c3697e166fb7f77b10d62211bcef7f74
Programming Hero <programming.hero1@gmail.com>
2022-04-06 17:11:24 -0500

    installed firebase deploy

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -19,9 +19,10 @@
 
 /**
  * Firebase hosting steps
- * 1. npm install -g firebase-tools
- * 2. firebase login
- * 3. firebase init
- * 4. firebase deploy
+ * 1. npm install -g firebase-tools (one time for your computer)
+ * 2. firebase login (one time for your computer)
+ * 3. firebase init (one time for each project )
+ * 4. npm run build (every time you want to deploy)
+ * 5. firebase deploy (every time you want to deploy)
  * 
 */
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -32,7 +32,7 @@ const Shipment = () => {
     return (
         <div className='form-container'>
             <div>
-                <h2 className='form-title'>Shipping Information</h2>
+                <h2 className='form-title'>Your Shipping Info</h2>
                 <form onSubmit={handleCreateUser}>
                     <div className="input-group">
                         <label htmlFor="name">Your Name</label>
